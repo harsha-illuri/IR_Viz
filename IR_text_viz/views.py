d6976e26c126f8d78610e11399ebb5cbff16705f
Harsha Illuri <harsha@datascience.dhcp.asu.edu>
2019-05-03 11:53:05 -0700

    fixed post issue with upload

--- IR_text_viz/views.py
+++ IR_text_viz/views.py
@@ -11,62 +11,66 @@ def predictAndVisualize():
     # use the prev functions and predict
     # merge with raw text inp file
     pass
-
+import shutil
 def upload(request):
-    def getContext():
-        folder = 'input/'
-        p = preprocess('input')
-        p.process_raw()
-        p.getTestData()
-
-        # predict
-        testob = Classifier_LSTM('test_data.h5')
-        df = testob.predict()
-        grouped = df.groupby(['line'])
-        data = []
-        for l, group in grouped:
-            line = {}
-            for i, row in group.iterrows():
-                line[i] = {
-                    "class" : row['predict'],
-                    "text" : row['text_x']
-                }
-            data.append(line)
-        grouped = df.groupby('predict')
-
-        classes = {}
-        total = 0
-        for key, group in grouped:
-            total += len(group)
-            classes[key] = len(group)
-
-        print(total)
-
-        for x in classes:
-            classes[x] = classes[x] * 100 / total
-
-        context =   {'data': data, 'classes':classes}
-        filehandler = open('context.pkl', 'wb')
-        pickle.dump(context, filehandler)
-    # getContext()
-    p = multiprocessing.Process(target=getContext)
-    p.start()
-    filehandler = open('context.pkl', 'rb')
-    context = pickle.load(filehandler)
-    # context = getContext()
-    return render(request, "textViewer.html", context)
-
-
-
     if request.method == 'POST' and request.FILES['myfile']:
+        shutil.rmtree('input')
+        os.mkdir('input')
         myfile = request.FILES['myfile']
-        fs = FileSystemStorage(location=folder)
+        fs = FileSystemStorage(location = 'input')
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
 
+        def getContext():
+            folder = 'input/'
+            p = preprocess('input')
+            p.process_raw()
+            p.getTestData()
 
-        return render(request, 'home.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'home.html')
+            # predict
+            testob = Classifier_LSTM('test_data.h5')
+            df = testob.predict()
+            grouped = df.groupby(['line'])
+            data = []
+            for l, group in grouped:
+                line = {}
+                for i, row in group.iterrows():
+                    line[i] = {
+                        "class": row['predict'],
+                        "text": row['text_x']
+                    }
+                data.append(line)
+            grouped = df.groupby('predict')
+
+            classes = {}
+            total = 0
+            for key, group in grouped:
+                total += len(group)
+                classes[key] = len(group)
+
+            print(total)
+
+            for x in classes:
+                classes[x] = classes[x] * 100 / total
+
+            context = {'data': data, 'classes': classes}
+            filehandler = open('context.pkl', 'wb')
+            pickle.dump(context, filehandler)
+
+        # getContext()
+        p = multiprocessing.Process(target=getContext)
+        p.start()
+        filehandler = open('context.pkl', 'rb')
+        context = pickle.load(filehandler)
+        # context = getContext()
+        return render(request, "textViewer.html", context)
+    else:
+        return render(request, 'home.html')
+
+
+
+
+
+
+
 
